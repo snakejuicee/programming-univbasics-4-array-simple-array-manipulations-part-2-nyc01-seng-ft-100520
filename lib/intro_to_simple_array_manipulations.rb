ccf4465ef7987d938f1ab0d49dc5defe85deb912
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-22 00:27:59 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+def using_concat(array1, array2)
+  
+ new_array = array1.concat(array2)
+  
+end
+
+def using_insert(array, element)
+  
+  array.insert(4, element)
+  
+end
+
+def using_uniq(array)
+  
+  new_array = array.uniq()
+  
+end
+
+def using_flatten(array)
+  
+  flat_array = array.flatten()
+  
+end
+
+def using_delete(array, string)
+  
+  array.delete(string)
+  
+end
+
+def using_delete_at(array, int)
+  
+  array.delete_at(int)
+  
+end
+
